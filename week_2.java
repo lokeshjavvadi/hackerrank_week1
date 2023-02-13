eb1df0381c6c5a0b4163accef4f6e6c20c04c7fd
lokeshjavvadi <85761767+lokeshjavvadi@users.noreply.github.com>
2023-02-13 14:56:59 +0530

    Create week_2.java

---
+++ week_2.java
@@ -0,0 +1,61 @@
+//Sales by Match
+class Result {
+    public static int sockMerchant(int n, List<Integer> ar) {
+        Collections.sort(ar);
+        int count=0;
+        int i=0;
+        while(i<n-1){
+            if(ar.get(i).equals(ar.get(i+1))){
+                count+=1;
+                i=i+2;
+                }
+            else
+                i=i+1;
+        }return count;
+    }
+
+}
+
+
+
+//Zig Zag Sequence
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner kb = new Scanner(System.in);
+        int test_cases = kb.nextInt();
+        for(int cs = 1; cs <= test_cases; cs++){
+            int n = kb.nextInt();
+             int a[] = new int[n];
+            for(int i = 0; i < n; i++){
+                a[i] = kb.nextInt();
+            }
+            findZigZagSequence(a, n);
+        }
+    }
+    public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = (n)/2;
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid + 1;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        for(int i = 0; i < n; i++){
+            if(i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+}
